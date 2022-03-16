b066264694435b44452154954c97b07c87f5a752
Kang Dain <dmelli0505@gmail.com>
2022-03-17 02:42:45 +0900

    firestore collection model

---
+++ inside_out/lib/models/sleepInfo.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Sleep {
+  Sleep({
+    required this.id,
+    required this.date,
+    required this.sleepingTime,
+    required this.bedTime,
+    required this.awakeTime,
+  });
+
+  final String id;
+  final Timestamp date;
+  final int sleepingTime;
+  final Timestamp bedTime;
+  final Timestamp awakeTime;
+
+  factory Sleep.fromMap(Map<String, dynamic> data, String id){
+    return Sleep(
+      id: id,
+      date: data['date'],
+      sleepingTime: data['sleepingTime'],
+      bedTime: data['bedTime'],
+      awakeTime: data['awakeTime']
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'date': date,
+      'sleepingTime': sleepingTime,
+      'bedTime': bedTime,
+      'awakeTime':awakeTime,
+    };
+  }
+}
+
---
+++ inside_out/lib/models/userInfo.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class UserInfo {
+  UserInfo({
+    required this.userId,
+    required this.email,
+    required this.sleepGoals,
+  });
+
+  final String userId;
+  final String email;
+  final GoalTime sleepGoals;
+
+  factory UserInfo.fromMap(Map<String, dynamic> data, String id){
+    return UserInfo(
+      userId: id,
+      email: data['email'],
+      sleepGoals: data['sleepGoals']
+    );
+  }
+}
+
+class GoalTime {
+  GoalTime({
+    required this.wakeupHours,
+    required this.wakeupMinutes,
+    required this.sleepHours,
+    required this.sleepMinutes,
+  });
+
+  final int wakeupHours;
+  final int wakeupMinutes;
+  final int sleepHours;
+  final int sleepMinutes;
+
+  factory GoalTime.fromMap(Map<int, dynamic> data, String id){
+    return GoalTime(
+      wakeupHours: data['wakeupHours'],
+      wakeupMinutes: data['wakeupMinutes'],
+      sleepHours: data['sleepHours'],
+      sleepMinutes: data['sleepMinutes'],
+    );
+  }
+}
